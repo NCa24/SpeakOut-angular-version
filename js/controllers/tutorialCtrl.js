fdbf730b2ad6b3cb677aed69bd5760df78636ffb
NCa24 <nelson.mrcardoso@gmail.com>
2017-08-06 14:24:50 +0100

    First commit

---
+++ js/controllers/tutorialCtrl.js
@@ -0,0 +1,10 @@
+var tutCtrl = angular.module("tutorialCtrlModule", [])
+
+.controller("TutorialCtrl", ["$scope", function($scope){
+	
+	$scope.name = "thomas"
+	//our programming work is done
+}]);
+
+
+
